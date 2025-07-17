d519cff3796356804cfbc6c4d94a08c94542d6fc
Veniamin Shapovalov <veniaminshp@gmail.com>
2025-07-17 18:48:00 +0600

    fix(yookassa): provider added in payments.module.ts

--- src/payments/payments.module.ts
+++ src/payments/payments.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common'
 import { YookassaController } from './controllers/yookasa.controller'
 import { YookassaService } from './services/yookassa.service'
 import { PaymentsService } from './services/payments.service'
+import { WebhookService } from './services/webhook.service'
 
 @Module({
   controllers: [YookassaController],
-  providers: [YookassaService, PaymentsService],
+  providers: [YookassaService, PaymentsService, WebhookService],
   exports: [YookassaService, PaymentsService],
 })
 export class PaymentsModule {}
