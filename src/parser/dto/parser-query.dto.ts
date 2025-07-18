c7874fecf92f918a670342e8dfdca1a45ceb5d01
Veniamin Shapovalov <veniaminshp@gmail.com>
2025-07-18 12:26:14 +0600

    chore: example url for parser changed

--- src/parser/dto/parser-query.dto.ts
+++ src/parser/dto/parser-query.dto.ts
@@ -6,7 +6,8 @@ export class ParserQueryDto {
 
   @ApiProperty({
     description: 'URL for parsing a specific page',
-    example: 'https://example.com/events',
+    example:
+      'https://www.afisha.ru/w/creations/performance/2487/f9f12827-da00-4200-80aa-b755ed5e3e51/https',
     type: String,
     format: 'url',
     required: false,
